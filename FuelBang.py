891d14709a9530995ca91dfc77e01e3f537e584c
SelfBiasedCode <70506200+SelfBiasedCode@users.noreply.github.com>
2020-09-08 09:04:08 +0200

    Established PEP8 conformity

--- FuelBang.py
+++ FuelBang.py
@@ -1,240 +1,240 @@
 from enum import Enum
 import pandas
 
+
 class Smoothing(Enum):
     Relative = 1
     RelativeLinear = 2
     Linear = 3
 
+
 class BangData:
-    def __init__(self, fullFuelRpm=1700, fullFuelPress=330, smoothingFactor=5.0,
-                 pressureWeight=0.5, smoothingMode=Smoothing.RelativeLinear):
-        self.fullFuelRpm = fullFuelRpm
-        self.fullFuelPress = fullFuelPress
-        self.smoothingFactor = smoothingFactor # reduction per delta
-        self.pressureWeight = pressureWeight # 0: smooth only over RPM, 1: smooth only over hPa
-        self.smoothingMode = smoothingMode
+    def __init__(self, full_fuel_rpm=1700, full_fuel_press=330, smoothing_factor=5.0,
+                 pressure_weight=0.5, smoothing_mode=Smoothing.RelativeLinear):
+        self.full_fuel_rpm = full_fuel_rpm
+        self.full_fuel_press = full_fuel_press
+        self.smoothing_factor = smoothing_factor  # reduction per delta
+        self.pressure_weight = pressure_weight  # 0: smooth only over RPM, 1: smooth only over hPa
+        self.smoothing_mode = smoothing_mode
 
-def smoothingRelative(inputData, maxRow, maxCol, bangData):
-    weightPressure = bangData.pressureWeight
-    weightRpm = (1 - bangData.pressureWeight)
 
-    for row in range(maxRow - 1, 0, -1):
-        for col in range(maxCol - 1, 1, -1): # omit the zero column
+def smoothing_relative(input_data, max_row, max_col, bang_data):
+    weight_pressure = bang_data.pressure_weight
+    weight_rpm = (1 - bang_data.pressure_weight)
+
+    for row in range(max_row - 1, 0, -1):
+        for col in range(max_col - 1, 1, -1):  # omit the zero column
             # get neighbours
-            neighbourPress = inputData.iloc[row, col + 1]
-            neighbourRpm = inputData.iloc[row + 1, col]
+            neighbour_press = input_data.iloc[row, col + 1]
+            neighbour_rpm = input_data.iloc[row + 1, col]
 
             # calculate value of change
-            valRpm = 0 if weightRpm == 0 else ((neighbourRpm / bangData.smoothingFactor) * weightRpm)
-            valPress = 0 if weightPressure == 0 else ((neighbourPress / bangData.smoothingFactor )* weightPressure)
+            val_rpm = 0 if weight_rpm == 0 else ((neighbour_rpm / bang_data.smoothing_factor) * weight_rpm)
+            val_press = 0 if weight_pressure == 0 else (
+                        (neighbour_press / bang_data.smoothing_factor) * weight_pressure)
 
             # apply weighted smoothing
-            fuelValue = (valPress  + valRpm) / 2
-            if (fuelValue < 0):
-                fuelValue = 0
+            fuel_value = (val_press + val_rpm) / 2
+            if fuel_value < 0:
+                fuel_value = 0
 
-            inputData.iloc[row, col] = int(fuelValue)
+            # round to full int and store
+            input_data.iloc[row, col] = int(fuel_value)
 
 
-def smoothingRelativeLinear(inputData, maxRow, maxCol, bangData):
-    smoothingFactorPressure = bangData.smoothingFactor * bangData.pressureWeight
-    smoothingFactorRpm = bangData.smoothingFactor * (1 - bangData.pressureWeight)
+def smoothing_relative_linear(input_data, max_row, max_col, bang_data):
+    smoothing_factor_pressure = bang_data.smoothing_factor * bang_data.pressure_weight
+    smoothing_factor_rpm = bang_data.smoothing_factor * (1 - bang_data.pressure_weight)
 
-    for row in range(maxRow - 1, 0, -1):
-        for col in range(maxCol - 1, 1, -1): # omit the zero column
+    for row in range(max_row - 1, 0, -1):
+        for col in range(max_col - 1, 1, -1):  # omit the zero column
             # get neighbours
-            neighbourPress = inputData.iloc[row, col + 1]
-            neighbourRpm = inputData.iloc[row + 1, col]
+            neighbour_press = input_data.iloc[row, col + 1]
+            neighbour_rpm = input_data.iloc[row + 1, col]
 
             # get magnitude of change
-            diffPress = inputData.iloc[0, col + 1] - inputData.iloc[0, col]
-            diffRpm = inputData.iloc[row, 0] - inputData.iloc[row + 1, 0]
+            diff_press = input_data.iloc[0, col + 1] - input_data.iloc[0, col]
+            diff_rpm = input_data.iloc[row, 0] - input_data.iloc[row + 1, 0]
 
             # calculate value of change
-            valRpm = neighbourRpm - (smoothingFactorRpm * diffRpm)
-            valPress = neighbourPress - (smoothingFactorPressure * diffPress)
+            val_rpm = neighbour_rpm - (smoothing_factor_rpm * diff_rpm)
+            val_press = neighbour_press - (smoothing_factor_pressure * diff_press)
 
             # apply weighted smoothing
-            fuelValue = (valPress  + valRpm) / 2
-            if (fuelValue < 0):
-                fuelValue = 0
+            fuel_value = (val_press + val_rpm) / 2
+            if fuel_value < 0:
+                fuel_value = 0
 
-            inputData.iloc[row, col] = int(fuelValue)
+            # round to full int and store
+            input_data.iloc[row, col] = int(fuel_value)
 
 
-def smoothingLinear(inputData, maxRow, maxCol, bangData):
-    smoothingFactorPressure = bangData.smoothingFactor * bangData.pressureWeight
-    smoothingFactorRpm = bangData.smoothingFactor * (1 - bangData.pressureWeight)
+def smoothing_linear(input_data, max_row, max_col, bang_data):
+    smoothing_factor_pressure = bang_data.smoothing_factor * bang_data.pressure_weight
+    smoothing_factor_rpm = bang_data.smoothing_factor * (1 - bang_data.pressure_weight)
 
-    for row in range(maxRow - 1, 0, -1):
-        for col in range(maxCol - 1, 1, -1): # omit the zero column
+    for row in range(max_row - 1, 0, -1):
+        for col in range(max_col - 1, 1, -1):  # omit the zero column
             # get neighbours
-            neighbourPress = inputData.iloc[row, maxCol]
-            neighbourRpm = inputData.iloc[maxRow, col]
+            neighbour_press = input_data.iloc[row, max_col]
+            neighbour_rpm = input_data.iloc[max_row, col]
 
             # get magnitude of change
-            diffPress = inputData.iloc[0, maxCol] - inputData.iloc[0, col]
-            diffRpm = inputData.iloc[row, 0] - inputData.iloc[maxRow, 0]
+            diff_press = input_data.iloc[0, max_col] - input_data.iloc[0, col]
+            diff_rpm = input_data.iloc[row, 0] - input_data.iloc[max_row, 0]
 
             # calculate value of change
-            valRpm = neighbourRpm - (smoothingFactorRpm * diffRpm)
-            valPress = neighbourPress - (smoothingFactorPressure * diffPress)
+            val_rpm = neighbour_rpm - (smoothing_factor_rpm * diff_rpm)
+            val_press = neighbour_press - (smoothing_factor_pressure * diff_press)
 
             # apply weighted smoothing
-            fuelValue = (valPress + valRpm) / 2
-            if (fuelValue < 0):
-                fuelValue = 0
+            fuel_value = (val_press + val_rpm) / 2
+            if fuel_value < 0:
+                fuel_value = 0
 
-            inputData.iloc[row, col] = int(fuelValue)
-
-
-def sanitizeInput(dataFrame):
-    # fix missing element
-    dataFrame[0][0] = 0
-    # delete empty column
-    # del dataFrame[21]
-
-def desanitizeInput(dataFrame):
-    # remove  missing element
-    dataFrame[0][0] = pandas.np.NaN
+            # round to full int and store
+            input_data.iloc[row, col] = int(fuel_value)
 
 
-def findLimits(inputData, bangData):
+def sanitize_input(data_frame):
+    # TuneECU exports miss the first element, so we have to insert it manually
+    data_frame[0][0] = 0
+
+
+def desanitize_input(data_frame):
+    # delete the previously added element to prevent import problems with TuneECU
+    data_frame[0][0] = pandas.np.NaN
+
+
+def find_limits(input_data, bang_data):
     # find lowest row
-    maxRow = 0  # exclusive
-    for i in range(1, len(inputData)):
-        currRpm = inputData[0][i]
-        if currRpm >= bangData.fullFuelRpm:
-            maxRow = i
+    max_row = 0  # exclusive
+    for i in range(1, len(input_data)):
+        curr_rpm = input_data[0][i]
+        if curr_rpm >= bang_data.full_fuel_rpm:
+            max_row = i
         else:
             break
 
-    maxCol = 0  # exclusive
-    for i in range(1, len(inputData.columns)):
-        currPress = inputData[i][0]
-        if currPress < bangData.fullFuelPress:
-            maxCol = i
+    max_col = 0  # exclusive
+    for i in range(1, len(input_data.columns)):
+        curr_press = input_data[i][0]
+        if curr_press < bang_data.full_fuel_press:
+            max_col = i
         else:
             break
 
-    return maxRow, maxCol
+    return max_row, max_col
 
-def processZeroAndSmooth(inputData, bangData):
-    maxRow, maxCol = findLimits(inputData, bangData)
+
+def process_zero_and_smooth(input_data, bang_data):
+    max_row, max_col = find_limits(input_data, bang_data)
 
     # insert zeroes
-    inputData.iloc[1:maxRow, 1:maxCol] = 0
+    input_data.iloc[1:max_row, 1:max_col] = 0
 
     # smoothing
-    if bangData.smoothingMode is Smoothing.Relative:
-        smoothingRelative(inputData, maxRow, maxCol, bangData)
-    elif bangData.smoothingMode is Smoothing.RelativeLinear:
-        smoothingRelativeLinear(inputData, maxRow, maxCol, bangData)
-    elif bangData.smoothingMode is Smoothing.Linear:
-        smoothingLinear(inputData, maxRow, maxCol, bangData)
+    if bang_data.smoothing_mode is Smoothing.Relative:
+        smoothing_relative(input_data, max_row, max_col, bang_data)
+    elif bang_data.smoothing_mode is Smoothing.RelativeLinear:
+        smoothing_relative_linear(input_data, max_row, max_col, bang_data)
+    elif bang_data.smoothing_mode is Smoothing.Linear:
+        smoothing_linear(input_data, max_row, max_col, bang_data)
     else:
         pass
 
 
-def processMultiply(inputData, bangData):
-    maxRow, maxCol = findLimits(inputData, bangData)
-    weightPressure = bangData.pressureWeight
-    weightRpm = (1 - bangData.pressureWeight)
+def process_multiply(input_data, bang_data):
+    max_row, max_col = find_limits(input_data, bang_data)
+    weight_pressure = bang_data.pressure_weight
+    weight_rpm = (1 - bang_data.pressure_weight)
 
-    for row in range(maxRow - 1, 0, -1):
-        for col in range(maxCol - 1, 0, -1):
-            fuelInput = inputData.iloc[row, col]
+    for row in range(max_row - 1, 0, -1):
+        for col in range(max_col - 1, 0, -1):
+            fuel_input = input_data.iloc[row, col]
 
             # get magnitude of change
-            diffPress = inputData.iloc[0, maxCol] - inputData.iloc[0, col]
-            diffRpm = inputData.iloc[row, 0] - inputData.iloc[maxRow, 0]
+            diff_press = input_data.iloc[0, max_col] - input_data.iloc[0, col]
+            diff_rpm = input_data.iloc[row, 0] - input_data.iloc[max_row, 0]
 
-            valPress = fuelInput / (bangData.smoothingFactor * diffPress)
-            valRpm = fuelInput / (bangData.smoothingFactor * diffRpm)
+            val_press = fuel_input / (bang_data.smoothing_factor * diff_press)
+            val_rpm = fuel_input / (bang_data.smoothing_factor * diff_rpm)
 
-            fuelValue = (valPress * weightPressure) + (weightRpm * valRpm)
+            fuel_value = (val_press * weight_pressure) + (weight_rpm * val_rpm)
 
+            input_data.iloc[row, col] = int(fuel_value)
 
-            inputData.iloc[row, col] = int(fuelValue)
 
 def difference(current, orig, relative):
     result = current.copy()
     for row in range(1, len(result)):
-        for col in range(1, len(result.columns)-1):
-            currVal = current.iloc[row, col]
-            origVal = orig.iloc[row, col]
+        for col in range(1, len(result.columns) - 1):
+            curr_val = current.iloc[row, col]
+            orig_val = orig.iloc[row, col]
             if relative:
-                if origVal == 0:
-                    if currVal > 0:
+                if orig_val == 0:
+                    if curr_val > 0:
                         result.iloc[row, col] = int(+100)
-                    elif currVal < 0:
+                    elif curr_val < 0:
                         result.iloc[row, col] = int(-100)
                     else:
                         result.iloc[row, col] = int(0)
                 else:
-                    result.iloc[row, col] = int(((currVal-origVal) / origVal) * 100)
+                    result.iloc[row, col] = int(((curr_val - orig_val) / orig_val) * 100)
             else:
-                result.iloc[row, col] = int(currVal - origVal)
+                result.iloc[row, col] = int(curr_val - orig_val)
 
     return result
 
+
 def main():
-    inputPathL1 = r"C:\path\to\file"
-    inputPathL2 = r"C:\path\to\file"
-    inputPathPrevComp = r"C:\path\to\file"
+    input_path_l1 = r"C:\path\to\file"
+    input_path_l2 = r"C:\path\to\file"
+    input_path_prev_comp = r"C:\path\to\file"
 
-    outputPathL1 = r"C:\path\to\file"
-    outputPathL2 = r"C:\path\to\file"
-    outputPathL1CompBaseAbs = r"C:\path\to\file"
-    outputPathL1CompPrevAbs = r"C:\path\to\file"
-    outputPathL1CompBaseRel = r"C:\path\to\file"
-    outputPathL1CompPrevRel = r"C:\path\to\file"
+    output_path_l1 = r"C:\path\to\file"
+    output_path_l2 = r"C:\path\to\file"
+    output_path_l1_comp_base_abs = r"C:\path\to\file"
+    output_path_l1_comp_prev_abs = r"C:\path\to\file"
+    output_path_l1_comp_base_rel = r"C:\path\to\file"
+    output_path_l1_comp_prev_rel = r"C:\path\to\file"
 
-    dataL1 = pandas.read_csv(inputPathL1, header=None, delimiter='\t', dtype="Int64")
-    dataL2 = pandas.read_csv(inputPathL2, header=None, delimiter='\t', dtype="Int64")
-    dataL1Baseline = pandas.read_csv(inputPathL1, header=None, delimiter='\t', dtype="Int64")
-    dataL1Prev = pandas.read_csv(inputPathPrevComp, header=None, delimiter='\t', dtype="Int64")
+    # read data
+    data_l1 = pandas.read_csv(input_path_l1, header=None, delimiter='\t', dtype="Int64")
+    data_l2 = pandas.read_csv(input_path_l2, header=None, delimiter='\t', dtype="Int64")
+    data_l1_baseline = pandas.read_csv(input_path_l1, header=None, delimiter='\t', dtype="Int64")
+    data_l1_prev = pandas.read_csv(input_path_prev_comp, header=None, delimiter='\t', dtype="Int64")
 
-    sanitizeInput(dataL1)
-    sanitizeInput(dataL2)
-    sanitizeInput(dataL1Prev)
+    # preprocess
+    sanitize_input(data_l1)
+    sanitize_input(data_l2)
+    sanitize_input(data_l1_prev)
 
-    bangData1_1 = BangData(fullFuelRpm=1700, fullFuelPress=330, smoothingFactor=7,
-                           pressureWeight=0.8, smoothingMode=Smoothing.RelativeLinear)
+    bang_data1_1 = BangData(full_fuel_rpm=1700, full_fuel_press=330, smoothing_factor=7,
+                            pressure_weight=0.8, smoothing_mode=Smoothing.RelativeLinear)
 
-    bangData1_2 = BangData(fullFuelRpm=1600, fullFuelPress=330, smoothingFactor=2.3,
-                           pressureWeight=0.05, smoothingMode=Smoothing.RelativeLinear)
+    # process
+    process_zero_and_smooth(data_l1, bang_data1_1)
+    process_zero_and_smooth(data_l2, bang_data1_1)
 
-    bangData2 = BangData(fullFuelRpm=1700, fullFuelPress=330, smoothingFactor=20,
-                         pressureWeight=1.0, smoothingMode=Smoothing.Linear)
+    # calculate absolute and relative differences
+    diff_baseline_abs = difference(data_l1, data_l1_baseline, False)
+    diff_prev_abs = difference(data_l1, data_l1_prev, False)
+    diff_baseline_rel = difference(data_l1, data_l1_baseline, True)
+    diff_prev_rel = difference(data_l1, data_l1_prev, True)
 
-    bangData3 = BangData(fullFuelRpm=1700, fullFuelPress=330, smoothingFactor=30,
-                         pressureWeight=1.0, smoothingMode=Smoothing.RelativeLinear)
+    # posstprocess data to make it importable
+    desanitize_input(data_l1)
+    desanitize_input(data_l2)
 
-    bangData4 = BangData(fullFuelRpm=1600, fullFuelPress=330, smoothingFactor=4,
-                         pressureWeight=0.7, smoothingMode=Smoothing.Linear)
+    # save data
+    data_l1.to_csv(output_path_l1, header=False, index=False, sep='\t')
+    data_l2.to_csv(output_path_l2, header=False, index=False, sep='\t')
+    diff_baseline_abs.to_csv(output_path_l1_comp_base_abs, header=False, index=False, sep='\t')
+    diff_prev_abs.to_csv(output_path_l1_comp_prev_abs, header=False, index=False, sep='\t')
+    diff_baseline_rel.to_csv(output_path_l1_comp_base_rel, header=False, index=False, sep='\t')
+    diff_prev_rel.to_csv(output_path_l1_comp_prev_rel, header=False, index=False, sep='\t')
 
-    bangData5 = BangData(fullFuelRpm=1700, fullFuelPress=330, smoothingFactor=1,
-                         pressureWeight=0.5, smoothingMode=Smoothing.Relative)
-
-    processZeroAndSmooth(dataL1, bangData1_1)
-    processZeroAndSmooth(dataL2, bangData1_1)
-
-    diffBaselineAbs = difference(dataL1, dataL1Baseline, False)
-    diffPrevAbs = difference(dataL1, dataL1Prev, False)
-    diffBaselineRel = difference(dataL1, dataL1Baseline, True)
-    diffPrevRel = difference(dataL1, dataL1Prev, True)
-
-    desanitizeInput(dataL1)
-    desanitizeInput(dataL2)
-
-    dataL1.to_csv(outputPathL1, header=False, index=False, sep='\t')
-    dataL2.to_csv(outputPathL2, header=False, index=False, sep='\t')
-    diffBaselineAbs.to_csv(outputPathL1CompBaseAbs, header=False, index=False, sep='\t')
-    diffPrevAbs.to_csv(outputPathL1CompPrevAbs, header=False, index=False, sep='\t')
-    diffBaselineRel.to_csv(outputPathL1CompBaseRel, header=False, index=False, sep='\t')
-    diffPrevRel.to_csv(outputPathL1CompPrevRel, header=False, index=False, sep='\t')
 
 if __name__ == '__main__':
     main()
